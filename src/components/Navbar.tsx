7cf285e35386bc5f5eff99c7b9164b86de37038a
fanny698 <sa.chamorro@duocuc.cl>
2025-11-04 21:26:33 -0300

    cree archivos y carpetas
    components: Navbar.tsx,ProductoCard.tsx,Carrito.tsx
    data: productos.js

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Disco Club</a>
+      </div>
+    </nav>
+  );
+}
---
+++ src/data/productos.js
@@ -0,0 +1,5 @@
+export const productos = [
+  { id: 1, nombre: "Entrada General", precio: 10000, imagen: "img/entrada.png" },
+  { id: 2, nombre: "Promo Cervezas", precio: 20000, imagen: "img/cervezas.png" },
+  { id: 3, nombre: "Promo Whisky", precio: 50000, imagen: "img/whisky.png" },
+];

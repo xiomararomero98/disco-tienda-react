25313f8c83fb57e2f3d2dd4beb3896459522c9c6
unknown <SSDD@PNAWSA04LC42309.duoc.acad>
2025-11-19 13:29:56 -0300

    lo que hizo el profe

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts'
+  }
 })

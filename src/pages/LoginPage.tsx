09b5c7f601fd6640a5738a6ad5d9b12e3b9ffd16
Renato Díaz <re.diazp@duocuc.cl>
2025-11-27 23:51:08 -0300

    login "Listo"

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Componente ProductosPage
 const LoginPage: React.FC = () => {
+  const[email, setEmail] = useState("");
+  const[password, setPassword] = useState("")
+  const[error, setError] = useState("")
+
+  const handleLogin = async(e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+
+    try{
+      const response = await fetch("http://localhost:8080/api/auth/login", {
+        method : "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify({email, password})
+      });
+
+      if(!response.ok){
+        setError("Credenciales invalidas");
+        return;
+      }
+
+      const data = await response.json();
+
+      localStorage.setItem("token", data.token);
+
+      window.location.href = "/dashboard";
+    }catch{
+      setError("Error al conectar con el servidor")
+    }
+  };
+  
+  
   return (
     <div>
-      <h1>Productos</h1>
-      {/* Contenido de la página */}
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Email:</label>
+          <input
+            type='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Contraseña:</label>
+          <input
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        {error && <p style={{color:"red"}}>{error}</p>}
+
+        <button type='submit'>Ingresar</button>
+      </form>
     </div>
   );
 };
 
-// Añade la exportación por defecto que falta
 export default LoginPage;

756918599b7cab13d1cc0163fc392c524ef1295d
Renato Díaz <re.diazp@duocuc.cl>
2025-11-28 04:05:53 -0300

    fix pagina registro

--- src/pages/RegistroPage.tsx
+++ src/pages/RegistroPage.tsx
@@ -35,41 +35,41 @@ const RegistroPage: React.FC = () => {
   }
   
   return (
-    <div>
-      <h1>Productos</h1>
-      <form onSubmit={handleRegister}>
-        <div>
-          <label>Email:</label>
-          <input
-            type='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+    <div className='registro-container'>
+      <div className='registro-box'>
+        <h2>Registro</h2>
+        <form onSubmit={handleRegister}>
+          <div>
+            <label>Email:</label>
+            <input
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
         </div>
         <div>
-          <label>Contraseña:</label>
-          <input
-            type='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+            <label>Contraseña:</label>
+            <input
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
         </div>
         <div>
-          <label>Usuario:</label>
-          <input
-            type='username'
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-      </form>
-
+            <label>Usuario:</label>
+            <input
+              type='username'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <button type='submit'>Registrarse</button>
+        </form>
         {error && <p style={{color:"red"}}>{error}</p>}
-
-        <button type='submit'>Ingresar</button>
+      </div>
     </div>
   );
 };

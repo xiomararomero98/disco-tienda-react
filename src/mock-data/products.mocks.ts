6502da4576d3aebc087ad27f955c9344a77a02ae
fanny698 <sa.chamorro@duocuc.cl>
2025-11-28 01:24:04 -0300

    hice funcionar las imagenes

--- src/mock-data/products.mocks.ts
+++ src/mock-data/products.mocks.ts
@@ -1,7 +1,10 @@
 import type { Product } from "../interfaces/product.interface";
+import entradaImg from "../assets/img/entradas.png";
+import cervezasImg from "../assets/img/cervezas.png";
+import whiskysImg from "../assets/img/whiskys.png";
 
 export const productosMock: Product[] = [
-  { id: 1, nombre: "Entrada General", precio: 10000, imagen: "/img/entradas.png", descripcion: "Acceso general a Disco Club." },
-  { id: 2, nombre: "Promo Balde de Cervezas", precio: 20000, imagen: "/img/cervezas.png", descripcion: "6 cervezas heladas para compartir." },
-  { id: 3, nombre: "Promo Balde de Whisky + bebidas", precio: 50000, imagen: "/img/whiskys.png", descripcion: "Whisky premium con mezclas para la noche." }
+  { id: 1, nombre: "Entrada General", precio: 10000, imagen: entradaImg, descripcion: "Acceso general a Disco Club." },
+  { id: 2, nombre: "Promo Balde de Cervezas", precio: 20000, imagen: cervezasImg, descripcion: "6 cervezas heladas para compartir." },
+  { id: 3, nombre: "Promo Balde de Whisky + bebidas", precio: 50000, imagen: whiskysImg, descripcion: "Whisky premium con mezclas para la noche." }
 ];
